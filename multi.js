6539c0caacf46c0e197c8bddabbdde7ce8deaf30
subhashini1998 <dommarasubhashini9032@gmail.com>
2025-10-08 08:26:49 +0530

    commiting the file

---
+++ multi.js
@@ -0,0 +1,11 @@
+const number = parseInt(prompt('Enter an integer: '));
+
+//creating a multiplication table
+for(let i = 1; i <= 10; i++) {
+
+    // multiply i with number
+    const result = i * number;
+
+    // display the result
+    console.log(`${number} * ${i} = ${result}`);
+}
